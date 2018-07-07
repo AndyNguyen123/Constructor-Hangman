82627bf03095790bb8ee9e4eb77b94ec45c5da52
ngbaoan <bambi_19912003@yahoo.com>
2018-07-06 18:16:54 -0700

    add promise to handle async API call

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const game = require('./Word.js');
 const inquirer = require('inquirer');
 const ud = require('urban-dictionary')
 
-const urbanWord = {};
-
-// getUrbanWord();
-function playGame() {
+function displayGameHeader(entry) {
     console.log(
-`-------------------------------------------------------------------------------
-In this game you will guess an "urban" word based on the definition provided.
-Good luck!!!!!
+        `-------------------------------------------------------------------------------
+        In this game you will guess an "urban" word based on the definition provided.
+        Good luck!!!!!
+        
+        Definition: ${entry.definition}
+        `)
+}
 
-Definition: ${urbanWord.definition}
-`)
+function inquirerGuessLetter() {
     inquirer.prompt([
         {
             name: "guessLetter",
@@ -21,27 +21,26 @@ Definition: ${urbanWord.definition}
     ])
         .then(function (answer) {
             console.log(answer.guessLetter);
-        })
+        });
 }
 
-function getUrbanWord () {
-    ud.random(function (error, entry) {
-      if (error) {
-        console.error(error.message)
-      } else {
-        urbanWord.word = entry.word;
-        urbanWord.definition = entry.definition;
-        urbanWord.example = entry.example;
-        console.log(urbanWord.definition);
-      }
-    })
+playGame();
+function playGame() {
+    return getUrbanWord()
+        .then((entry) => displayGameHeader(entry))
+        .catch((error) => console.error(error));
 };
 
 
-const prom = new Promise( (res, rej) => {
-     res(getUrbanWord())
-     rej(err => console.log(err))
-})
-
-prom.then( foo => console.log(foo))
-.catch(err => console.log(err))
+function getUrbanWord() {
+    return new Promise((resolve, reject) => {
+        return ud.random(function (error, entry) {
+            if (error) {
+                reject(error.message);
+            } else {
+                // console.log(entry);
+                resolve(entry);
+            };
+        });
+    });
+};
