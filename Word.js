1bc76c777bf52a4e011d7c40b96f8167e886c6cd
Andy Nguyen <anbao.ng@gmail.com>
2018-07-08 14:27:05 -0700

    revised promises

--- Word.js
+++ Word.js
@@ -1,20 +1,16 @@
 const Letter = require('./Letter.js');
 
-function Word (word) {
+function Word(word) {
     this.letterArray = word.split('').map(element => new Letter(element));
 }
 Word.prototype.displayWord = function () {
-   return this.letterArray.map(element => element.displayLetter()).join(' ');
+    return this.letterArray.map(element => element.displayLetter()).join(' ');
 }
 Word.prototype.checkAllLetter = function (letterInput) {
-    this.letterArray.map(element => element.checker(letterInput));
+    return this.letterArray.forEach(element => element.checker(letterInput));
 }
 
 module.exports = Word;
-
-let chosenWord = new Word('andeptrai');
-chosenWord.checkAllLetter('a');
-console.log(chosenWord.displayWord());
 
 
 
--- index.js
+++ index.js
@@ -4,46 +4,38 @@ const inquirer = require('inquirer');
 const ud = require('urban-dictionary')
 
 
-function displayGameHeader(entry) {
+function displayGameHeaderWith(definition) {
     console.log(
         `
 -------------------------------------------------------------------------------
 In this game you will guess an "urban" word based on the definition provided.
 Good luck!!!!!
 
-Definition: ${entry.definition}
+Definition: ${definition}
         `)
 };
 
-function inquirerGuessLetter() {
-    inquirer.prompt([
-        {
-            name: "guessLetter",
-            message: "Guess a letter!"
-        }
-    ])
-        .then(function (answer) {
-            console.log(answer.guessLetter);
-        });
+function letterGuessed() {
+    return new Promise((resolve, reject) => {
+        return inquirer
+            .prompt([
+                {
+                    name: "guessLetter",
+                    message: "Guess a letter!"
+                }
+            ])
+            .then(function (answer) {
+                resolve(answer.guessLetter);
+            });
+    });
 };
 
-function displayWordHolder(word) {
+function displayWordHolderOf(word) {
     const chosenWord = new Word(word);
-    return chosenWord.displayWord();
-}
-
-
-function playGame() {
-    return getUrbanWord()
-        .then((entry) => {
-            displayGameHeader(entry);
-            console.log(displayWordHolder(entry.word));
-            inquirerGuessLetter();
-
-        })
-        .catch((error) => console.error(error));
+    const display = chosenWord.displayWord();
+    console.log(display);
+    return display;
 };
-
 
 function getUrbanWord() {
     return new Promise((resolve, reject) => {
@@ -57,4 +49,26 @@ function getUrbanWord() {
     });
 };
 
+function playGame() {
+    let urbanWord = '', definition = '', example = '';
+    return getUrbanWord()
+        .then((entry) => {
+
+            urbanWord = entry.word;
+            definition = entry.definition;
+            example = entry.example;
+
+            displayGameHeaderWith(definition);
+            displayWordHolderOf(urbanWord);
+            letterGuessed()
+                .then((letter) => {
+                    if()
+                    new Word(urbanWord).checkAllLetter(letter);
+                    displayWordHolderOf(urbanWord);
+                })
+                .catch((err) => console.error(err));
+
+        })
+        .catch((err) => console.error(err));
+};
 playGame();
